f1c4655aaa4d62c635f98054cbdfdf2ecce9c973
mertgulerx <108007620+mertgulerx@users.noreply.github.com>
2025-02-20 14:46:46 +0300

    add SignupRequest and MessageResponse

---
+++ src/main/java/com/mertg/cinemia/security/response/MessageResponse.java
@@ -0,0 +1,12 @@
+package com.mertg.cinemia.security.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class MessageResponse {
+    private String message;
+
+
+}
